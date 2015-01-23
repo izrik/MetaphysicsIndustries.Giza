fc99071f45b1c01bd90d94d01c670d14c965a653
izrik <izrik@izrik.com>
2015-01-23 11:24:52 -0600

    Get the name of a directive as a string.

--- DefinitionDirective.cs
+++ DefinitionDirective.cs
@@ -16,4 +16,30 @@ namespace MetaphysicsIndustries.Giza
         Subtoken,
         Comment,
     }
+
+    public static class DefinitionDirectiveHelper
+    {
+        public static string GetName(this DefinitionDirective directive)
+        {
+            switch (directive)
+            {
+            case DefinitionDirective.MindWhitespace:
+                return "mind whitespace";
+            case DefinitionDirective.IgnoreCase:
+                return "ignore case";
+            case DefinitionDirective.Atomic:
+                return "atomic";
+            case DefinitionDirective.Token:
+                return "token";
+            case DefinitionDirective.Subtoken:
+                return "subtoken";
+            case DefinitionDirective.Comment:
+                return "comment";
+            }
+
+            throw new ArgumentOutOfRangeException("directive",
+                string.Format("Unknown DefinitionDirective value: {0}",
+                    (int)directive));
+        }
+    }
 }
